853ab55ae911cf8d9e83d0d07e388df2202837df
rohitghatal1 <rohitghatal@gmail.com>
2025-09-14 22:36:46 +0545

    added sidebar

--- src/Components/MainScreen.tsx
+++ src/Components/MainScreen.tsx
@@ -1,7 +1,13 @@
+import Sidebar from "./Sidebar";
+
 const MainScreen = () => {
   return (
     <div>
       <h2>Main Screen</h2>
+      <div>
+        <Sidebar />
+      </div>
+      <div>Main Content</div>
     </div>
   );
 };
